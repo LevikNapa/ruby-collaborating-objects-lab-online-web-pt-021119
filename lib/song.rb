0c9b287a44b6922103f197c46c59bece3bd3d5da
Levik Naparstek <levik010@gmail.com>
2019-03-19 20:45:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,10 +9,9 @@ class Song
   def self.all 
     @@all 
   end
-  def artist_name
-    if self.artist 
-      self.artist.name 
-    end
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end
   def self.new_by_filename(file)
     song_info = file.chomp(".mp3").split(" - ")
