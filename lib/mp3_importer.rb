1e0fe6ddca364accba63c626b239ca90d7a50b4d
Levik Naparstek <levik010@gmail.com>
2019-03-19 20:38:34 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -3,5 +3,11 @@ class MP3Importer
   def initialize(path)
     @path = path
   end
-  
+  def files
+    files = []
+    Dir.new(self.path).each do |file|
+      files << file if file.length > 4
+    end
+    files
+  end
 end
